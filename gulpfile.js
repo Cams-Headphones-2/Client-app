dba9e5875bfc0fb5d791761fbed3857bc30f28ab
Max Stern <stern.maxwell@gmail.com>
2016-04-18 09:23:03 -0500

    Gulpfile added & Less watching.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp  = require('gulp');
+var less  = require('gulp-less');
+var watch = require('gulp-watch');
+
+gulp.task('hello', function(){
+  console.log('Gulp, I am thirsty!');
+})
+
+gulp.task('less', function(){
+  return gulp.src('public/less/**/*.less')
+    .pipe(less()) // Using gulp-less
+    .pipe(gulp.dest('public/stylesheets/'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('public/less/**/*.less', ['less']);
+  // Other watchers
+})
+
+gulp.task('default', ['less', 'watch', 'hello']);
