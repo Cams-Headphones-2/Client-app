273b481f2aa7303dbed7318817176bf634b444b9
cam iverson <camiver@gmail.com>
2016-04-19 14:40:27 -0500

    updated controllers with userID session stuff to import into DB when user makes a chart

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var User          = require('../models/user'),
+    Chart         = require('../models/chart'),
+    express       = require('express'),
+    router        = express.Router();
 
   // ------------------ GET account home ----------------------------
 router.get('/', function(req, res, next) {
-  res.render('account', { title: "My account" });
+  if(req.session.loggedIn === true) {
+    res.render('account', { title: "My account" });
+  } else res.redirect('/login');
 })    // ------------------ GET edit account -------------------------
 .get('/edit', function(req, res, next) {
-  res.render('profile-edit', { title: "Edit my account" });
+  if(req.session.loggedIn === true) {
+    res.render('profile-edit', { title: "Edit my account" });
+  } else res.redirect('/login');
 });
 
 module.exports = router;
