caa493445ba0cd12f48d43ac880d127f7258ff2b
cam iverson <camiver@gmail.com>
2016-04-20 16:52:38 -0500

    get route for Mike to pull from

--- controllers/index.js
+++ controllers/index.js
@@ -108,15 +108,6 @@ router.get('/', function(req, res, next) {
 
   // } else res.redirect('/login');
 })
-
-.get('/getChart', function(req, res, next) {
-  Chart.findOne({ _id: "5717a60158d2867e1c5d6f10"}, function(err, chart) {
-    if (chart) {
-      res.send(chart);
-    } else console.log('chart exists already');
-  })
-})
-
 .get('/chartviewer', function(req, res, next) {
     res.render('chart-viewer', { title: 'View a Chart' });
 })
