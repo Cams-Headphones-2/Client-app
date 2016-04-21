925d0b39625ee3a1d6ed875cd23a00efec515119
Michael Sturrus <msturrus@gmail.com>
2016-04-20 19:52:35 -0500

    headway on accounts

--- public/jsx/account.js
+++ public/jsx/account.js
@@ -14,14 +14,14 @@ var React     = require('react'),
 
 var UserChart = React.createClass({
       getInitialState: function(){
-        return {charts: [], imgURL: "", artist: "", album: ""}
+        return {charts: []}
       },
 
       componentDidMount: function(){
           //  this.props.items = [];
            var self = this;
            $.ajax({
-              url: '/tacos',
+              url: '/account/accountCharts',
               type: 'get',
               dataType: 'json',
               success: function(response){
@@ -29,10 +29,10 @@ var UserChart = React.createClass({
                 console.log("here's your damn object")
                 // return self.state.items = response;
 
-                response.forEach(function(serv){
+                response.forEach(function(chart){
                   // state = self.state;
-                  self.state.items.push(serv);
-                  console.log(self.state.items)
+                  self.state.charts.push(chart);
+                  console.log(self.state.charts)
                   self.setState();
                   // self.setState(state);
 
@@ -44,106 +44,17 @@ var UserChart = React.createClass({
                 console.log(err)
               }
             })
-            console.log(self.state.items)
+            console.log(self.state.charts)
          },
 
-      handlechartsearchChange: function(event){
-        console.log(event.target.value)
-        var state = this.state;
-        state.chartsearch = event.target.value;
-        this.setState(state);
-        console.log(this.state)
-      },
-
-      handleimgURLChange: function(event){
-        console.log(event.target.value)
-        var state = this.state;
-        state.imgURL = event.target.value;
-        this.setState(state);
-        console.log(this.state)
-      },
-      handleartistChange: function(event){
-        console.log(event.target.value)
-        var state = this.state;
-        state.artist = event.target.value;
-        this.setState(state);
-
-        console.log(this.state)
-      },
-      handlealbumChange: function(event){
-        console.log(event.target.value)
-        var state = this.state;
-        state.album = event.target.value;
-        this.setState(state);
-
-        console.log(this.state)
-      },
-      handleSubmit: function(event){
-        event.preventDefault();
-        var self = this;
-        var cache = new LastFMCache();
-        console.log(this)
-
-        var lastfm = new LastFM({
-              apiKey    : 'f21088bf9097b49ad4e7f487abab981e',
-              apiSecret : '7ccaec2093e33cded282ec7bc81c6fca',
-              cache     : cache
-            });
-        lastfm.album.search({album: self.state.chartsearch}, {success: function(data){
-              // console.log(data);
-              // console.log(data.results.albummatches.album[0].name);
-              // console.log(data.results.albummatches.album[0].image[2]["#text"]);
-              // console.log(data.results.albummatches.album[0].artist);
-
-              document.getElementById('results-zone').innerHTML = "";
-              document.getElementById('search-box').val = "g";
-
-
-              var state = self.state;
-              var albumInfo = {
-                albumName: data.results.albummatches.album[0].name,
-                albumCover: data.results.albummatches.album[0].image[2]["#text"],
-                albumArtist: data.results.albummatches.album[0].artist
-              }
-
-              state.charts.push(albumInfo);
-              var albumName = data.results.albummatches.album[0].name;
-              var albumCover = data.results.albummatches.album[0].image[2]["#text"];
-              var albumArtist = data.results.albummatches.album[0].artist;
-
-              state.imgURL = albumCover;
-              state.artist = albumArtist;
-              state.album = albumName;
-
-              self.setState(state);
-
-              console.log(self.state.charts);
-
-              // var ChartDiv = $('<div draggable="true" class="album-div" style="height: 248px; width: 176px; border: 1px dashed; background-color: lightgreen"></div>');
-              // $(ChartDiv).append('<img draggable="false" src ="' + albumCover + '">');
-              // $(ChartDiv).append('<p>' + albumName + '</p>');
-              // $(ChartDiv).append('<p>' + albumArtist + '</p>');
-              // $('#results-appender').append(ChartDiv);
-
-
-            }, error: function(code, message){
-            /* Show error message. */
-            }});
-      },
       render: function(){
         return(
-          <div id="results-container">
-            <form className="UserChart" onSubmit={this.handleSubmit}>
-              <input id="search-box" type="text" placeholder="Search for an Album" onChange={this.handlechartsearchChange} value={this.state.chartsearch}/>
-              <button className="btn btn-primary" type="submit" value="post">Search</button>
-            </form>
-            <div id='results-zone'>
+          <div id="charts-list">
             {
-              this.state.charts.map(function(album, i){
-                return <ChartDiv albumCover={album.albumCover} album={album.albumName} artist={album.albumArtist} location={1} key={i} />
+              this.state.charts.map(function(chart, i){
+                return <ChartDiv chartName={chart.nameOfChart} chartID={chart.id} key={i} />
               }.bind(this))
             }
-            </div>
           </div>
 
         )
@@ -154,14 +65,8 @@ var UserChart = React.createClass({
       render: function() {
         return (
           <div draggable="true" data-location="3" className="album-div">
-            <img className="album-cover" src={this.props.albumCover} draggable="false" />
-            <input type="hidden" name="albumCover" value={this.props.albumCover} />
-            <p className="album-name">{this.props.album}</p>
-            <input type="hidden" name="album" value={this.props.album} />
-            <p className="artist-name">{this.props.artist}</p>
-            <input type="hidden" name="artist" value={this.props.artist} />
-            <input type="hidden" name="key" value={this.props.key} />
-            <input type="hidden" name="location" value={this.props.location} />
+            <p className="chart-name">{this.props.chartName}</p>
+            <input type="hidden" name="chart-ID" value={this.props.chartID} />
 
 
           </div>
