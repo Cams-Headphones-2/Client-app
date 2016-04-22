92db93dc4b51fdb2813978e2fa8fee58009ee447
Michael Sturrus <msturrus@gmail.com>
2016-04-22 09:30:41 -0500

    readme documentation is the mark of a conscientous developer and I hate it

--- controllers/index.js
+++ controllers/index.js
@@ -11,11 +11,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '/Mu/sic Chart Generator | Create Your Own Music Flowcharts' });
 }) // ------------------ GET logout -------------------------
 .get('/logout', function(req, res, next) {
+  if (req.session.loggedIn === true) {
   req.session.loggedIn = null;
   req.session.currentUserId = null;
   req.session.currentUser = null;
   console.log('You have been logged out.');
   res.redirect('/');
+  }
 }) // ------------------ GET register ------------------------
 .get('/register', function(req, res, next) {
   if (req.session.loggedIn === true) {
